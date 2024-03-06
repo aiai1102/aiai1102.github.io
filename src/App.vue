be4ebb4d28d141e2738392fdef7d8c4f774549d5
山﨑 凌 <yama1897@esco.co.jp>
2024-03-07 01:01:10 +0900

    'new'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <HelloWorld msg="ﾃｽﾄ"/></template>
+  <HelloWorld msg="ﾃｽﾄ①"/></template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
